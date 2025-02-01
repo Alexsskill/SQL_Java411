74a62f8320bb613fe431eb156929f553f48496c2
Alexsskill <harckovaleks@yandex.ru>
2025-02-01 22:53:08 +0400

    do CRUD operations with Customer

--- src/Lesson1.sql
+++ src/Lesson1.sql
@@ -1,20 +1,43 @@
 CREATE TABLE Product(
-                        "ProductID" BIGINT,
-                        "ProductName" VARCHAR(100),
-                        "Price" DECIMAL(10,2)
+            "productID" BIGINT GENERATED ALWAYS AS IDENTITY,
+            "productName" VARCHAR(100),
+            "price" DECIMAL(10,2)
 );
 
 CREATE TABLE Customer(
-                         "CustomerID" BIGINT,
-                         "FirstName" VARCHAR(50),
-                         "LastName" VARCHAR(50),
-                         Email VARCHAR(90)
+            "customerID" BIGINT GENERATED ALWAYS AS IDENTITY,
+            "firstName" VARCHAR(50),
+            "lastName" VARCHAR(50),
+            email VARCHAR(90),
+            city VARCHAR(50)
 );
 
 CREATE TABLE Order(
-                      "OrderID" BIGINT,
-                      "CustomerID" BIGINT,
-                      "ProductID" BIGINT,
-                      "Quantity" BIGINT,
-                      "OrderDate" DATE
+            "orderID" BIGINT GENERATED ALWAYS AS IDENTITY,
+            "customerID" BIGINT,
+            "productID" BIGINT,
+            "quantity" BIGINT,
+            "orderDate" DATE
 );
+
+INSERT INTO Customer (firstName, lastName, email, city) VALUES
+            ('Сергеев', 'Сергей', 'sergeev@yandex.ru', 'Москва'),
+            ('Александров', 'Александр', 'alex@yandex.ru','Cургут'),
+            ('Иванов', 'Иван', 'ivanov@yandex.ru', 'Ленинград');
+
+UPDATE Customer
+SET lastName = 'Максим'
+WHERE customerID = 1;
+
+UPDATE Customer
+SET city = 'Санкт-Петербург'
+WHERE city = 'Ленинград';
+
+DELETE FROM Customer
+WHERE customerID = 2;
+
+SELECT * FROM Customer;
+
+SELECT * FROM Customer
+WHERE city = 'Санкт-Петербург'
+ORDER BY lastName;
